3781fdd96415d6b6d6401475bb3804696b71a8c3
Roberto Casaluce <35006139+rcasaluce@users.noreply.github.com>
2024-05-05 09:02:09 +0200

    Add files via upload

---
+++ runMV.sh
@@ -0,0 +1,8 @@
+#java -jar multivesta.jar -c -m MV_python_integrator.py -sm true -f stepsAtSteps.multiquatex -l 1 -sots 1 -sd vesta.python.simpy.SimPyState -vp true -bs 30 -ds [10] -a 0.05 -otherParams "/Users/andrea/Documents/python_envs/env_research/bin/python3"
+
+
+java -jar multivesta.jar -c -m MV_python_integrator.py -sm true -f avgQuantities.multiquatex -l 4 -sots 1 -sd vesta.python.simpy.SimPyState -vp true -bs 30 -ds [5,5,5] -a 0.05 -otherParams "python3" -ir 1
+
+
+
+
